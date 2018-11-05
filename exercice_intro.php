7944c5034df4b435446310ee846bee9fc4c081ce
jouiniwael <44771623+jouiniwael@users.noreply.github.com>
2018-11-05 12:12:19 +0100

    Add files via upload
    checkpoint one first commit

---
+++ exercice_intro.php
@@ -0,0 +1,12 @@
+<!doctype >
+<head>
+<title>
+exercice introduction 
+</title>
+</head>
+<body>
+<?php echo "<p>ceci est une page créée uniquement en PHP</p>" ;
+      echo "<p>ceci est la 2eme ligne créée en PHP</p>"; ?>
+ 
+<?php  echo'<a href="https://gomycode.tn/" > gomycode</a>'; ?>
+</body>
